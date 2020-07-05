3e9a7ab656a800862acde27b804766ce458e796a
Abhishek Jha <abhi_jha1995@rediffmail.com>
2020-07-05 23:35:40 +0530

    Changes for showing paginated by score; We already had paginated by date

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
-    let base_url = "http://localhost:9001/api/v1/paginate/date/";
+    let url_for_paginate_date = "http://localhost:9001/api/v1/paginate/date/";
+    let url_for_paginate_score = "http://localhost:9001/api/v1/paginate/score/";
     let prevpage = document.querySelector(".prevpage");
     let nextpage = document.querySelector(".nextpage");
     let countforward = 1, countbackward = 1;
+    let date_message = "Please enter a date in DDMMYYYY format:";
+    let score_message = "Please enter a score value";
+    let current_url = url_for_paginate_date;
+    let defaultLowerlimitScore = 4000;
 
     function getDate() {
         let today = new Date();
@@ -20,9 +25,9 @@ $(document).ready(function () {
         return today;
     }
 
-    function doAJAX(date, count) {
+    function doAJAX(url, count) {
         $.ajax({
-            url: base_url + date,
+            url: url,
             type: "GET",
             crossDomain: true,
             dataType: 'json',
@@ -88,24 +93,56 @@ $(document).ready(function () {
         $('#dataTable').replaceWith(tableHeader + tableData + "</table>");
     }
 
+    $("#getarticles").click(function (event) {
+        countforward = 1, countbackward = 1;
+        doAJAX(current_url + $("#txtDate").val(), countforward);//Called when Get Articles button clicked
+        event.preventDefault();
+    });
+
+    nextpage.addEventListener('click', function (event) {
+        countforward += 10;
+        doAJAX(event.target.title, countforward);
+    });
+    prevpage.addEventListener('click', function (event) {
+        countbackward = countforward - 10;
+        countforward = countbackward;
+        doAJAX(event.target.title, countbackward);
+    });
+
+    getbyDate = document.querySelector("#getbyDate");
+    getByScore = document.querySelector("#getByScore");
+    messagetodisplay = document.querySelector("#messagetodisplay span");
+
+
+    getbyDate.addEventListener('click', function(event){
+        event.target.style.backgroundColor = "green";
+        getByScore.style.backgroundColor = "white"
+        messagetodisplay.innerText = date_message;
+        let date = getDate();
+        txtDate = document.querySelector("#txtDate");
+        txtDate.value = date;
+        current_url = url_for_paginate_date;
+        doAJAX(current_url + date, countforward);//Call AJAX. Another calling point.
+    })
+
+    getByScore.addEventListener('click', function(event){
+        event.target.style.backgroundColor = "green";
+        getbyDate.style.backgroundColor = "white";
+        messagetodisplay.innerText = score_message;
+        txtDate = document.querySelector("#txtDate");
+        txtDate.value = defaultLowerlimitScore;
+        current_url = url_for_paginate_score;
+        doAJAX(current_url + txtDate.value, countforward);//Call AJAX. Another calling point.
+    })
+
+
     function runthis() {
         let date = getDate();
+        messagetodisplay.innerText = date_message;
+        getbyDate.style.backgroundColor = "red";
+        getByScore.style.backgroundColor = "white";
         $("#txtDate").attr("value", date);
-        doAJAX(date, countforward);//Call AJAX. This is the first calling point.
-        $("#getarticles").click(function (event) {
-            countforward = 1, countbackward = 1;
-            doAJAX($("#txtDate").val(), countforward);//Called when Get Articles button clicked
-            event.preventDefault();
-        });
-        nextpage.addEventListener('click', function (event) {
-            countforward += 10;
-            doAJAX(event.target.title.split("date/")[1], countforward);
-        });
-        prevpage.addEventListener('click', function (event) {
-            countbackward = countforward - 10;
-            countforward = countbackward;
-            doAJAX(event.target.title.split("date/")[1], countbackward);
-        });
+        doAJAX(url_for_paginate_date + date, countforward);//Call AJAX. This is the first calling point.
     }
 
     runthis();
